4049578355bc62ab43886e497aefdf2e48f2f258
haaleylew <66989529+haaleylew@users.noreply.github.com>
2020-11-11 02:17:57 -0700

    got info to append to cards

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 moment().format('L');
 
-//-----------------------Search Function for Current City Weather----------------------------------//
+
 function searchCity(cityname) {
 
     var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityname + "&units=imperial&appid=ecc0be5fd92206da3aa90cc41c13ca56";
@@ -12,12 +12,12 @@ function searchCity(cityname) {
     }).then(function (response) {
         console.log(response);
         console.log(queryURL);
-        //empty divs and ids that we need to dump content into.....
+        //divs for content
         $("#current").empty();
         var mainDate = moment().format('L');
 
 
-        //create HTML for city information......
+        //city info
         var cityNameEl = $("<h2>").text(response.name);
         var displayMainDate = cityNameEl.append(" " + mainDate);
         var tempEL = $("<p>").text("Tempraturer: " + response.main.temp);
@@ -43,14 +43,14 @@ function searchCity(cityname) {
             var currentIcon = $('<img>').attr("src", "http://openweathermap.org/img/wn/13d.png");
             currentIcon.attr("style", "height: 60px; width: 60px");
         }
-        //create HTML div to append new elements to render on page....
+
         var newDiv = $('<div>');
 
         newDiv.append(displayMainDate, currentIcon, tempEL, humEl, windEl);
 
         $("#current").html(newDiv);
 
-        //--------------------------------------------- UV call ---------------------------------------//
+
 
         var lat = response.coord.lat;
         var lon = response.coord.lon;
@@ -71,28 +71,28 @@ function searchCity(cityname) {
     });
 
 
-    //--------------------------------------------5 Day frocast call ---------------------------------------//
+
 
     $.ajax({
         url: queryURLforcast,
         method: 'GET'
     }).then(function (response) {
-        // Storing an array of results in the results variable
+
         var results = response.list;
-        //empty 5day div--------
+
         $("#5day").empty();
-        //create HTML for 5day forcast................
+        //create HTML for 5day forcast
         for (var i = 0; i < results.length; i += 8) {
             // Creating a div
             var fiveDayDiv = $("<div class='card shadow-lg text-white bg-primary mx-auto mb-10 p-2' style='width: 8.5rem; height: 11rem;'>");
 
-            //Storing the responses date temp and humidity.......
+            //Storing the responses
             var date = results[i].dt_txt;
             var setD = date.substr(0, 10)
             var temp = results[i].main.temp;
             var hum = results[i].main.humidity;
 
-            //creating tags with the result items information.....
+
             var h5date = $("<h5 class='card-title'>").text(setD);
             var pTemp = $("<p class='card-text'>").text("Temp: " + temp);;
             var pHum = $("<p class='card-text'>").text("Humidity " + hum);;
@@ -119,7 +119,7 @@ function searchCity(cityname) {
                 icon.attr("style", "height: 40px; width: 40px");
             }
 
-            //append items to.......
+            //append items to
             fiveDayDiv.append(h5date);
             fiveDayDiv.append(icon);
             fiveDayDiv.append(pTemp);
@@ -133,15 +133,15 @@ function searchCity(cityname) {
 
 }
 pageLoad();
-//----------------------------------------Event handler for user city search-----------------------//
+
 
 $("#select-city").on("click", function (event) {
-    // Preventing the button from trying to submit the form......
+
     event.preventDefault();
-    // Storing the city name........
+
     var cityInput = $("#city-input").val().trim();
 
-    //save search term to local storage.....
+    //save search term to local storage
     var textContent = $(this).siblings("input").val();
     var storearr = [];
     storearr.push(textContent);
@@ -151,7 +151,7 @@ $("#select-city").on("click", function (event) {
     pageLoad();
 });
 
-//---------------------------Call stored items on page load-------------------------------------//
+
 function pageLoad() {
     var lastSearch = JSON.parse(localStorage.getItem("cityName"));
     var searchDiv = $("<button class='btn border text-muted mt-1 shadow-sm bg-white rounded' style='width: 12rem;'>").text(lastSearch);
@@ -160,7 +160,7 @@ function pageLoad() {
     $("#searchhistory").prepend(psearch);
 }
 
-//Event deligation...
+
 $("#searchhistory").on('click', '.btn', function (event) {
     event.preventDefault();
     console.log($(this).text());
